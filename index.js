9ff1b1c0ce427a214579bf7f8236eeda5b312dc4
Miller <millerandressolis@gmail.com>
2024-09-29 09:48:23 -0700

    fix port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 // const PORT = 8080;
-const PORT: process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
